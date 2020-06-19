bc7a2c8b5ea1a15056fbc9f64c6d425291f1ec65
Adam Connolly <adam.connolly66@gmail.com>
2020-06-18 22:06:49 -0500

    gitignore

--- Recruiter_Manager/wwwroot/js/site.js
+++ Recruiter_Manager/wwwroot/js/site.js
@@ -1,32 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-//function getAllSalaries() {
-//    var dict = {
-//        city = this["city"].value,
-//        jobTitle = this["jobTitle"].value
-//    }
-//    $.get("http://localhost:55746/api/averagesalaries", function (data) {
-//        for (let i = 0; i < data.length; i++) {
-//            $("#salaryDisplay").append(
-//                `<tr>
-//                <td><strong>City</strong><td>
-//                <td><strong>Job Title</strong><td>
-//                <td><strong>Average Salary</strong><td>`
-//            );
-//            for (let i = 0; i < data.length; i++) {
-//                $("#salaryDisplay").append(`<tr>
-//                <td>${JSON.stringify(data[i].city).replace(/\"/g, '')}</td>
-//                <td>${JSON.stringify(data[i].jobTitle).replace(/\"/g, '')}</td>
-//                <td>${JSON.stringify(data[i].avgSalary).replace(/\"/g, '')}</td></tr>`
-//                )
-//            }
-
-//        }
-//    })
-//} (jQuery);
-    function getSalariesByTitle(e) {
+﻿function getSalariesByTitle(e) {
         var jobTitle = $("#JobTitle").val();
         $.get("http://localhost:55746/api/AverageSalaries/" + jobTitle, function (data) {
             console.log(data);
